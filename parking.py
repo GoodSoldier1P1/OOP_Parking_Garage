ee00f48b75076284fad07ffad77b09f4834a50ba
GoodSoldier1P1 <danielamyx859@gmail.com>
2023-10-20 20:38:33 -0400

    Basic Layout

---
+++ parking.py
@@ -0,0 +1,20 @@
+class ParkingGarage():
+    def __init__(self) -> None:
+        self.tickets = []
+        self.parkingSpaces = []
+        self.currentTicket = {}
+
+    
+    def takeTicket(self):
+        pass    # felt
+
+
+    def payForTicket(self):
+        pass    # cute
+
+
+    def leaveGarage(self):
+        pass    # WILL
+
+    def runner(self):
+        pass    # delete
